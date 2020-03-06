07335abbe9aaab30012529011bc32b11206f25ce
Resfa <faresamiar1@gmail.com>
2020-03-06 14:54:49 +0100

    Test api

--- app/src/main/java/com/rapguessr/MainActivity.java
+++ app/src/main/java/com/rapguessr/MainActivity.java
@@ -2,6 +2,9 @@ package com.rapguessr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import core.GLA;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GLA gla = new GLA();
+        String lyrics = gla.search("Ninho");
+        TextView tv = findViewById(R.id.textView);
+
+        tv.setText(lyrics);
     }
 }
