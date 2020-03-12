5863d7bbf8dd300c6ef79fb8c89fbfcd5838e11b
Resfa <faresamiar1@gmail.com>
2020-03-12 16:00:48 +0100

    Facile + difficile

--- app/src/main/java/com/rapguessr/ChoixRapperActivity.java
+++ app/src/main/java/com/rapguessr/ChoixRapperActivity.java
@@ -7,23 +7,36 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChoixRapperActivity extends AppCompatActivity {
+
+    private List<String> rappeurs;
+    private EditText etRappeur;
+    private RadioGroup radioGroup;
+    private int cptRappeurs;
+    private RadioButton radFacile, radDifficile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_rapper);
 
-        EditText etRappeur = findViewById(R.id.txtRappeur);
-        RadioGroup radioGroup = findViewById(R.id.rdGroup);
+        rappeurs = new ArrayList<>();
+        etRappeur = findViewById(R.id.txtRappeur);
+        radioGroup = findViewById(R.id.rdGroup);
+        radFacile = findViewById(R.id.radFacile);
+        radDifficile = findViewById(R.id.radDifficile);
 
         Button btnRappeur = findViewById(R.id.btnRappeur);
         Button btnConfirmer = findViewById(R.id.btnDemarrer);
 
-        int cptRappeur = 0;
+        cptRappeurs = 0;
 
         btnRappeur.setOnClickListener( new View.OnClickListener() {
             @Override public void onClick(View view) {
@@ -34,21 +47,34 @@ public class ChoixRapperActivity extends AppCompatActivity {
 
         btnConfirmer.setOnClickListener( new View.OnClickListener() {
             @Override public void onClick(View view) {
-
-                Intent i = new Intent();
-                startActivity(i);
+                demarrerJeu();
             }
         }
         );
-
     }
 
     private void demarrerJeu() {
-
+        if(cptRappeurs <= 5 && cptRappeurs >=1) {
+            if(radFacile.isChecked()) {
+                Intent i = new Intent(this, JeuFacileActivity.class);
+                i.putStringArrayListExtra("Rappeurs", (ArrayList<String>) rappeurs);
+                startActivity(i);
+            }
+            else {
+                Intent i = new Intent(this, JeuDifficileActivity.class);
+                i.putStringArrayListExtra("Rappeurs", (ArrayList<String>) rappeurs);
+                startActivity(i);
+            }
+        }
+        else
+            Toast.makeText(getApplicationContext(),"Il faut sélectionner 1 rappeur minimum",Toast.LENGTH_SHORT);
     }
 
     private void btnRappeurListener() {
-
+            if (cptRappeurs < 5)
+                rappeurs.add(etRappeur.getText().toString());
+            else
+                Toast.makeText(getApplicationContext(),"Vous avez déja ajouté 5 rappeurs",Toast.LENGTH_SHORT);
     }
 
 }
--- app/src/main/java/com/rapguessr/MainActivity.java
+++ app/src/main/java/com/rapguessr/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button jouer = findViewById(R.id.btnJouer);
+        Button btnJouer = findViewById(R.id.btnJouer);
 
-        Intent i = new Intent(this,ChoixRapperActivity.class);
-        startActivity(i);
+        btnJouer.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,ChoixRapperActivity.class);
+                        startActivity(i);
+                    }
+        });
 
     }
 }
