7208b3b9fdeada67d8497a341800d1adcf48f118
Yanisdz93 <55578164+Yanisdz93@users.noreply.github.com>
2020-03-12 16:02:23 +0100

    Installation firebase

---
+++ app/src/main/java/com/rapguessr/User.java
@@ -0,0 +1,31 @@
+package com.rapguessr;
+
+import java.util.ArrayList;
+
+public class User {
+
+    String pseudo;
+    int score;
+    ArrayList<String> rappeurs;
+
+    public User(){}
+
+
+    public User(String pseudo, int score, ArrayList<String> rappeurs) {
+        this.pseudo = pseudo;
+        this.score = score;
+        this.rappeurs = rappeurs;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public ArrayList<String> getRappeurs() {
+        return rappeurs;
+    }
+}
